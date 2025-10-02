1628ec467f1f482ecc5d7231bb92ab40615e6dfa
Lesego916 <segobuyisile@gmail.com>
2025-10-02 23:32:29 +0200

    BE

---
+++ api_project/settings.py
@@ -0,0 +1,25 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Added apps
+    'rest_framework',
+    'api',
+]
+
+INSTALLED_APPS += [
+    'rest_framework.authtoken',
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}

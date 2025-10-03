56218e4d7e1ff1028069545cfe0c9467d7b690d5
Lesego916 <segobuyisile@gmail.com>
2025-10-03 11:20:17 +0200

    BE

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -13,22 +13,22 @@ class BookAPITests(APITestCase):
     def test_list_books(self):
         url = reverse('book-list')
         response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, status.HTTP_200_OK)
 
     def test_create_book_authenticated(self):
         self.client.login(username="tester", password="pass123")
         url = reverse('book-list')
         data = {"title": "New Book", "publication_year": 2021, "author": self.author.id}
         response = self.client.post(url, data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, status.HTTP_201_CREATED)
 
     def test_create_book_unauthenticated(self):
         url = reverse('book-list')
         data = {"title": "Fail Book", "publication_year": 2021, "author": self.author.id}
         response = self.client.post(url, data)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.data, status.HTTP_403_FORBIDDEN)
 
     def test_filter_books(self):
         url = reverse('book-list') + '?search=Book'
         response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, status.HTTP_200_OK)
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,16 +1,16 @@
-from rest_framework import generics, permissions
+from django_filters import rest_framework as django_filters
 from rest_framework import filters
-from .models import Book
-from .serializers import BookSerializer
 
 class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-    search_fields = ['title', 'author__name']
-    ordering_fields = ['title', 'publication_year']
+    filter_backends = [django_filters.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ["title", "author", "publication_year"]
+    search_fields = ["title", "author__name"]
+    ordering_fields = ["title", "publication_year"]
+
 
 
 class BookDetail(generics.RetrieveUpdateDestroyAPIView):
